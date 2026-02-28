ff2c5b2f7c25a930b6debb0ffe8b2c8559b4604c
inksha <x1a010@163.com>
2026-02-28 11:18:14 +0800

    feat: add hash command

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,19 @@ import (
 	"os"
 
 	"github.com/InkShaStudio/go-command"
+	"github.com/inksha/sumi/internal/sumi_hash"
 )
 
 func Execute() {
-	cmd := command.RegisterCommand(
-		command.NewCommand("sumi").
-			ChangeDescription("Always want to see summer in you eyes.").
-			RegisterHandler(func(cmd *command.SCommand) {
-				fmt.Println("Hello, Sumi!")
-			}),
-	)
+	sumi := command.NewCommand("sumi").
+		ChangeDescription("Always want to see summer in you eyes.").
+		RegisterHandler(func(cmd *command.SCommand) {
+			fmt.Println("Hello, Sumi!")
+		})
+
+	sumi.AddSubCommand(sumi_hash.RegisterCommand())
+
+	cmd := command.RegisterCommand(sumi)
 
 	if err := cmd.Execute(); err != nil {
 		fmt.Println(err)
